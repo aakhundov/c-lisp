a9d16dfcd5851f96275c6d60ea88f3852f44cdb9
Adnan Akhundov <adnan.akhundov@argmax.ai>
2022-05-29 18:05:02 +0200

    Extract repl from main.c.

--- main.c
+++ main.c
@@ -1,41 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include "edit.h"
-
-static const char* exit_commands[] = {"exit", "quit", "q"};
-static const size_t n_exit_commands = sizeof(exit_commands) / sizeof(char*);
-
-static int is_exit_command(char* line) {
-    for (size_t i = 0; i < n_exit_commands; i++) {
-        if (strcmp(line, exit_commands[i]) == 0) {
-            return 1;
-        }
-    }
-
-    return 0;
-}
+#include "repl.h"
 
 int main(int argc, char** argv) {
-    puts("CLisp version 0.0.1");
-    puts("Type \"quit\" to quit\n");
-
-    int stop = 0;
-    while (!stop) {
-        char* input = readline("clisp> ");
-
-        if (is_exit_command(input)) {
-            stop = 1;
-        } else {
-            add_history(input);
-            printf("echo: \"%s\"\n", input);
-        }
-
-        free(input);
-    }
-
-    printf("\nBye!\n");
+    run_repl();
 
     return 0;
 }
